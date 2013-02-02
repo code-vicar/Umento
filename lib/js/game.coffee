a8ce55276c03b71c01608503700e9613898bc3c2
Scott Vickers <scott.w.vickers@gmail.com>
2013-02-01 19:13:33 -0500

    add game art and sound assets

--- lib/js/game.coffee
+++ lib/js/game.coffee
@@ -2,10 +2,29 @@ $ ->
   
   ns = window.Umento = window.Umento || {}
   
-  Crafty = window.Crafty;
+  Crafty = window.Crafty
   
   Crafty.init()
   
-  Crafty.background("green");
+  Crafty.background("green")
   
-  Crafty.e("2D, DOM, Color, Fourway").attr({w: 50, h: 50, x: 0, y: 0}).color("#EEE").fourway(5);
+  #Crafty.e("2D, DOM, Color, Fourway").attr({w: 50, h: 50, x: 0, y: 0}).color("#EEE").fourway(5)
+  SPRITESIZE = 32
+  Crafty.sprite(SPRITESIZE,'/game/art/PathAndObjects.png', { grass:[0,0], grass2:[1,0], grass3:[0,1], grass4:[1,1] })
+  
+  Crafty.e("2D, DOM, grass").attr
+    x:0
+    y:0
+    
+  Crafty.e("2D, DOM, grass2").attr
+    x:SPRITESIZE
+    y:0
+  Crafty.e("2D, DOM, grass2").attr
+    x:SPRITESIZE*2
+    y:0
+  Crafty.e("2D, DOM, grass3").attr
+    x:0
+    y:SPRITESIZE
+  Crafty.e("2D, DOM, grass4").attr
+    x:SPRITESIZE
+    y:SPRITESIZE
--- public/js/game.js
+++ public/js/game.js
@@ -1,17 +1,38 @@
 (function() {
 
   $(function() {
-    var Crafty, ns;
+    var Crafty, SPRITESIZE, ns;
     ns = window.Umento = window.Umento || {};
     Crafty = window.Crafty;
     Crafty.init();
     Crafty.background("green");
-    return Crafty.e("2D, DOM, Color, Fourway").attr({
-      w: 50,
-      h: 50,
+    SPRITESIZE = 32;
+    Crafty.sprite(SPRITESIZE, '/game/art/PathAndObjects.png', {
+      grass: [0, 0],
+      grass2: [1, 0],
+      grass3: [0, 1],
+      grass4: [1, 1]
+    });
+    Crafty.e("2D, DOM, grass").attr({
       x: 0,
       y: 0
-    }).color("#EEE").fourway(5);
+    });
+    Crafty.e("2D, DOM, grass2").attr({
+      x: SPRITESIZE,
+      y: 0
+    });
+    Crafty.e("2D, DOM, grass2").attr({
+      x: SPRITESIZE * 2,
+      y: 0
+    });
+    Crafty.e("2D, DOM, grass3").attr({
+      x: 0,
+      y: SPRITESIZE
+    });
+    return Crafty.e("2D, DOM, grass4").attr({
+      x: SPRITESIZE,
+      y: SPRITESIZE
+    });
   });
 
 }).call(this);
