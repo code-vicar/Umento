5852396cdb117e054bfe0696af924174a6b38200
Scott Vickers <scott.w.vickers@gmail.com>
2013-02-03 13:41:26 -0500

    add loading scene, move main into a scene

--- lib/js/game.coffee
+++ lib/js/game.coffee
@@ -6,113 +6,134 @@ $ ->
   
   Crafty.init()
   
-  Crafty.background("green")
+  updateLoading = (e)->
+    console.log "loading update"
+    console.log e
   
-  Crafty.load [
-    "/game/art/PathAndObjects.png"
-    "/game/art/Player.png"
-    "/game/sound/SomewhereSunny.mp3"
-    "/game/sound/SomewhereSunny.ogg"
-    "/game/sound/SomewhereSunny.wav"
-  ], ->
-    mainScene()
+  alertError = (e)->
+    alert "there was an error loading game assets"
+    console.log "loading error"
+    console.log e
+  
+  Crafty.scene "loading", ->
+    
+    #black background with some loading text
+    Crafty.background "#000"
+    Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 }).text("Loading").css({ "color":"#FFF", "text-align": "center" });
+  
+    Crafty.load [
+      "/game/art/PathAndObjects.png"
+      "/game/art/Player.png"
+      "/game/sound/SomewhereSunny.mp3"
+      "/game/sound/SomewhereSunny.ogg"
+      "/game/sound/SomewhereSunny.wav"
+    ], ->
+      Crafty.scene "main"
+    , (e)->
+      #progress function
+      updateLoading e
+    , (e)->
+      #error function
+      alertError e
 
-mainScene = ->
-  
-  SPRITESIZE = 32
-  LOGS = 10
-  
-  Crafty.c('LeftControls', {
-    init:->
-      @requires('Multiway')
-      return
-      
-    leftControls:(speed)->
-      @multiway speed,
-        W: -90
-        S: 90
-        D: 0
-        A: 180
-      return @
-  })
-  
-  Crafty.c('Human', {
-    init:->
-      @requires("SpriteAnimation, Collision, Grid")
-      @animate("walk",0,0,4)
-      
-      @bind 'NewDirection', (direction)->
-        #animate walking left
-        if direction.x < 0
-          @stop().animate("walk",15,-1) unless @isPlaying "walk"
-        #animate walking right
-        if direction.x > 0
-          @stop().animate("walk",15,-1) unless @isPlaying "walk"
-        #animate walking up
-        if direction.y < 0
-          @stop().animate("walk",15,-1) unless @isPlaying "walk"
-        #animate walking down
-        if direction.y > 0
-          @stop().animate("walk",15,-1) unless @isPlaying "walk"
-        #stop walking animation
-        if not direction.x and not direction.y
-          @stop()
-      
-      #check if there was a collision after each move event
-      @bind 'Moved', (from)->
-        if @hit('solid')
-          @attr
-            x:from.x
-            y:from.y
-      return
-  })
-  
-  Crafty.sprite(SPRITESIZE,"/game/art/PathAndObjects.png",
-  {
-    grass:[1,11],
-    log:[6,10],
-    pot:[12,11,1,2]
-  })
-  Crafty.sprite(36,"/game/art/Player.png",
-  {
-    player:[0,0,1,2]
-  })
-  
-  Crafty.audio.add
-    lounge:["/game/sound/SomewhereSunny.mp3", "/game/sound/SomewhereSunny.ogg", "/game/sound/SomewhereSunny.wav"]
+  Crafty.scene "main", ->
     
-  Crafty.audio.play('lounge',-1,1)
-  
-  scenemap = { h:0, w:0, axis:[] }
-  
-  scenemap.w = Math.ceil(Crafty.stage.elem.clientWidth/SPRITESIZE)
-  scenemap.h = Math.ceil(Crafty.stage.elem.clientHeight/SPRITESIZE)
-  
-  drawBackground = (xaxis, yaxis)->
-    posX = xaxis*SPRITESIZE
-    posY = yaxis*SPRITESIZE
-    #initialize the yaxis at this xaxis location
-    scenemap.axis[xaxis] = [] unless scenemap.axis[xaxis]?
-    scenemap.axis[xaxis][yaxis] = [] unless scenemap.axis[xaxis][yaxis]?
-    scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, grass").attr({x:posX,y:posY}))
+    SPRITESIZE = 32
+    LOGS = 10
     
-  addLog = ->
-    xaxis = Crafty.math.randomInt(0,(scenemap.w-1))
-    yaxis = Crafty.math.randomInt(0,(scenemap.h-1))
-    posX = xaxis*SPRITESIZE
-    posY = yaxis*SPRITESIZE
-    scenemap.axis[xaxis] = [] unless scenemap.axis[xaxis]?
-    scenemap.axis[xaxis][yaxis] = [] unless scenemap.axis[xaxis][yaxis]?
-    scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, log").attr({x:posX,y:posY}))
+    Crafty.c('LeftControls', {
+      init:->
+        @requires('Multiway')
+        return
+        
+      leftControls:(speed)->
+        @multiway speed,
+          W: -90
+          S: 90
+          D: 0
+          A: 180
+        return @
+    })
+    
+    Crafty.c('Human', {
+      init:->
+        @requires("SpriteAnimation, Collision, Grid")
+        @animate("walk",0,0,4)
+        
+        @bind 'NewDirection', (direction)->
+          #animate walking left
+          if direction.x < 0
+            @stop().animate("walk",15,-1) unless @isPlaying "walk"
+          #animate walking right
+          if direction.x > 0
+            @stop().animate("walk",15,-1) unless @isPlaying "walk"
+          #animate walking up
+          if direction.y < 0
+            @stop().animate("walk",15,-1) unless @isPlaying "walk"
+          #animate walking down
+          if direction.y > 0
+            @stop().animate("walk",15,-1) unless @isPlaying "walk"
+          #stop walking animation
+          if not direction.x and not direction.y
+            @stop()
+        
+        #check if there was a collision after each move event
+        @bind 'Moved', (from)->
+          if @hit('solid')
+            @attr
+              x:from.x
+              y:from.y
+        return
+    })
+    
+    Crafty.sprite(SPRITESIZE,"/game/art/PathAndObjects.png",
+    {
+      grass:[1,11],
+      log:[6,10],
+      pot:[12,11,1,2]
+    })
+    Crafty.sprite(36,"/game/art/Player.png",
+    {
+      player:[0,0,1,2]
+    })
+    
+    Crafty.audio.add
+      lounge:["/game/sound/SomewhereSunny.mp3", "/game/sound/SomewhereSunny.ogg", "/game/sound/SomewhereSunny.wav"]
       
-  for x in [0..(scenemap.w-1)]
-    do (x) ->
-      drawBackground x,y for y in [0..(scenemap.h-1)]
+    Crafty.audio.play('lounge',-1,1)
+    
+    scenemap = { h:0, w:0, axis:[] }
+    
+    scenemap.w = Math.ceil(Crafty.stage.elem.clientWidth/SPRITESIZE)
+    scenemap.h = Math.ceil(Crafty.stage.elem.clientHeight/SPRITESIZE)
+    
+    drawBackground = (xaxis, yaxis)->
+      posX = xaxis*SPRITESIZE
+      posY = yaxis*SPRITESIZE
+      #initialize the yaxis at this xaxis location
+      scenemap.axis[xaxis] = [] unless scenemap.axis[xaxis]?
+      scenemap.axis[xaxis][yaxis] = [] unless scenemap.axis[xaxis][yaxis]?
+      scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, grass").attr({x:posX,y:posY}))
+      
+    addLog = ->
+      xaxis = Crafty.math.randomInt(0,(scenemap.w-1))
+      yaxis = Crafty.math.randomInt(0,(scenemap.h-1))
+      posX = xaxis*SPRITESIZE
+      posY = yaxis*SPRITESIZE
+      scenemap.axis[xaxis] = [] unless scenemap.axis[xaxis]?
+      scenemap.axis[xaxis][yaxis] = [] unless scenemap.axis[xaxis][yaxis]?
+      scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, log").attr({x:posX,y:posY}))
+        
+    for x in [0..(scenemap.w-1)]
+      do (x) ->
+        drawBackground x,y for y in [0..(scenemap.h-1)]
+    
+    (->
+      logs = LOGS+1
+      addLog() while (logs -= 1)
+    )()
+    
+    Crafty.e("2D, DOM, player, Human, LeftControls").attr({x: 0, y: 0, z:10}).leftControls(1)
   
-  (->
-    logs = LOGS+1
-    addLog() while (logs -= 1)
-  )()
-  
-  Crafty.e("2D, DOM, player, Human, LeftControls").attr({x: 0, y: 0, z:10}).leftControls(1)
-  
+  Crafty.scene "loading"
+    
--- public/js/game.js
+++ public/js/game.js
@@ -1,150 +1,173 @@
 (function() {
-  var mainScene;
 
   $(function() {
-    var Crafty, ns;
+    var Crafty, alertError, ns, updateLoading;
     ns = window.Umento = window.Umento || {};
     Crafty = window.Crafty;
     Crafty.init();
-    Crafty.background("green");
-    return Crafty.load(["/game/art/PathAndObjects.png", "/game/art/Player.png", "/game/sound/SomewhereSunny.mp3", "/game/sound/SomewhereSunny.ogg", "/game/sound/SomewhereSunny.wav"], function() {
-      return mainScene();
+    updateLoading = function(e) {
+      console.log("loading update");
+      return console.log(e);
+    };
+    alertError = function(e) {
+      alert("there was an error loading game assets");
+      console.log("loading error");
+      return console.log(e);
+    };
+    Crafty.scene("loading", function() {
+      Crafty.background("#000");
+      Crafty.e("2D, DOM, Text").attr({
+        w: 100,
+        h: 20,
+        x: 150,
+        y: 120
+      }).text("Loading").css({
+        "color": "#FFF",
+        "text-align": "center"
+      });
+      return Crafty.load(["/game/art/PathAndObjects.png", "/game/art/Player.png", "/game/sound/SomewhereSunny.mp3", "/game/sound/SomewhereSunny.ogg", "/game/sound/SomewhereSunny.wav"], function() {
+        return Crafty.scene("main");
+      }, function(e) {
+        return updateLoading(e);
+      }, function(e) {
+        return alertError(e);
+      });
     });
+    Crafty.scene("main", function() {
+      var LOGS, SPRITESIZE, addLog, drawBackground, scenemap, x, _fn, _i, _ref;
+      SPRITESIZE = 32;
+      LOGS = 10;
+      Crafty.c('LeftControls', {
+        init: function() {
+          this.requires('Multiway');
+        },
+        leftControls: function(speed) {
+          this.multiway(speed, {
+            W: -90,
+            S: 90,
+            D: 0,
+            A: 180
+          });
+          return this;
+        }
+      });
+      Crafty.c('Human', {
+        init: function() {
+          this.requires("SpriteAnimation, Collision, Grid");
+          this.animate("walk", 0, 0, 4);
+          this.bind('NewDirection', function(direction) {
+            if (direction.x < 0) {
+              if (!this.isPlaying("walk")) {
+                this.stop().animate("walk", 15, -1);
+              }
+            }
+            if (direction.x > 0) {
+              if (!this.isPlaying("walk")) {
+                this.stop().animate("walk", 15, -1);
+              }
+            }
+            if (direction.y < 0) {
+              if (!this.isPlaying("walk")) {
+                this.stop().animate("walk", 15, -1);
+              }
+            }
+            if (direction.y > 0) {
+              if (!this.isPlaying("walk")) {
+                this.stop().animate("walk", 15, -1);
+              }
+            }
+            if (!direction.x && !direction.y) {
+              return this.stop();
+            }
+          });
+          this.bind('Moved', function(from) {
+            if (this.hit('solid')) {
+              return this.attr({
+                x: from.x,
+                y: from.y
+              });
+            }
+          });
+        }
+      });
+      Crafty.sprite(SPRITESIZE, "/game/art/PathAndObjects.png", {
+        grass: [1, 11],
+        log: [6, 10],
+        pot: [12, 11, 1, 2]
+      });
+      Crafty.sprite(36, "/game/art/Player.png", {
+        player: [0, 0, 1, 2]
+      });
+      Crafty.audio.add({
+        lounge: ["/game/sound/SomewhereSunny.mp3", "/game/sound/SomewhereSunny.ogg", "/game/sound/SomewhereSunny.wav"]
+      });
+      Crafty.audio.play('lounge', -1, 1);
+      scenemap = {
+        h: 0,
+        w: 0,
+        axis: []
+      };
+      scenemap.w = Math.ceil(Crafty.stage.elem.clientWidth / SPRITESIZE);
+      scenemap.h = Math.ceil(Crafty.stage.elem.clientHeight / SPRITESIZE);
+      drawBackground = function(xaxis, yaxis) {
+        var posX, posY;
+        posX = xaxis * SPRITESIZE;
+        posY = yaxis * SPRITESIZE;
+        if (scenemap.axis[xaxis] == null) {
+          scenemap.axis[xaxis] = [];
+        }
+        if (scenemap.axis[xaxis][yaxis] == null) {
+          scenemap.axis[xaxis][yaxis] = [];
+        }
+        return scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, grass").attr({
+          x: posX,
+          y: posY
+        }));
+      };
+      addLog = function() {
+        var posX, posY, xaxis, yaxis;
+        xaxis = Crafty.math.randomInt(0, scenemap.w - 1);
+        yaxis = Crafty.math.randomInt(0, scenemap.h - 1);
+        posX = xaxis * SPRITESIZE;
+        posY = yaxis * SPRITESIZE;
+        if (scenemap.axis[xaxis] == null) {
+          scenemap.axis[xaxis] = [];
+        }
+        if (scenemap.axis[xaxis][yaxis] == null) {
+          scenemap.axis[xaxis][yaxis] = [];
+        }
+        return scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, log").attr({
+          x: posX,
+          y: posY
+        }));
+      };
+      _fn = function(x) {
+        var y, _j, _ref1, _results;
+        _results = [];
+        for (y = _j = 0, _ref1 = scenemap.h - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; y = 0 <= _ref1 ? ++_j : --_j) {
+          _results.push(drawBackground(x, y));
+        }
+        return _results;
+      };
+      for (x = _i = 0, _ref = scenemap.w - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; x = 0 <= _ref ? ++_i : --_i) {
+        _fn(x);
+      }
+      (function() {
+        var logs, _results;
+        logs = LOGS + 1;
+        _results = [];
+        while ((logs -= 1)) {
+          _results.push(addLog());
+        }
+        return _results;
+      })();
+      return Crafty.e("2D, DOM, player, Human, LeftControls").attr({
+        x: 0,
+        y: 0,
+        z: 10
+      }).leftControls(1);
+    });
+    return Crafty.scene("loading");
   });
-
-  mainScene = function() {
-    var LOGS, SPRITESIZE, addLog, drawBackground, scenemap, x, _fn, _i, _ref;
-    SPRITESIZE = 32;
-    LOGS = 10;
-    Crafty.c('LeftControls', {
-      init: function() {
-        this.requires('Multiway');
-      },
-      leftControls: function(speed) {
-        this.multiway(speed, {
-          W: -90,
-          S: 90,
-          D: 0,
-          A: 180
-        });
-        return this;
-      }
-    });
-    Crafty.c('Human', {
-      init: function() {
-        this.requires("SpriteAnimation, Collision, Grid");
-        this.animate("walk", 0, 0, 4);
-        this.bind('NewDirection', function(direction) {
-          if (direction.x < 0) {
-            if (!this.isPlaying("walk")) {
-              this.stop().animate("walk", 15, -1);
-            }
-          }
-          if (direction.x > 0) {
-            if (!this.isPlaying("walk")) {
-              this.stop().animate("walk", 15, -1);
-            }
-          }
-          if (direction.y < 0) {
-            if (!this.isPlaying("walk")) {
-              this.stop().animate("walk", 15, -1);
-            }
-          }
-          if (direction.y > 0) {
-            if (!this.isPlaying("walk")) {
-              this.stop().animate("walk", 15, -1);
-            }
-          }
-          if (!direction.x && !direction.y) {
-            return this.stop();
-          }
-        });
-        this.bind('Moved', function(from) {
-          if (this.hit('solid')) {
-            return this.attr({
-              x: from.x,
-              y: from.y
-            });
-          }
-        });
-      }
-    });
-    Crafty.sprite(SPRITESIZE, "/game/art/PathAndObjects.png", {
-      grass: [1, 11],
-      log: [6, 10],
-      pot: [12, 11, 1, 2]
-    });
-    Crafty.sprite(36, "/game/art/Player.png", {
-      player: [0, 0, 1, 2]
-    });
-    Crafty.audio.add({
-      lounge: ["/game/sound/SomewhereSunny.mp3", "/game/sound/SomewhereSunny.ogg", "/game/sound/SomewhereSunny.wav"]
-    });
-    Crafty.audio.play('lounge', -1, 1);
-    scenemap = {
-      h: 0,
-      w: 0,
-      axis: []
-    };
-    scenemap.w = Math.ceil(Crafty.stage.elem.clientWidth / SPRITESIZE);
-    scenemap.h = Math.ceil(Crafty.stage.elem.clientHeight / SPRITESIZE);
-    drawBackground = function(xaxis, yaxis) {
-      var posX, posY;
-      posX = xaxis * SPRITESIZE;
-      posY = yaxis * SPRITESIZE;
-      if (scenemap.axis[xaxis] == null) {
-        scenemap.axis[xaxis] = [];
-      }
-      if (scenemap.axis[xaxis][yaxis] == null) {
-        scenemap.axis[xaxis][yaxis] = [];
-      }
-      return scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, grass").attr({
-        x: posX,
-        y: posY
-      }));
-    };
-    addLog = function() {
-      var posX, posY, xaxis, yaxis;
-      xaxis = Crafty.math.randomInt(0, scenemap.w - 1);
-      yaxis = Crafty.math.randomInt(0, scenemap.h - 1);
-      posX = xaxis * SPRITESIZE;
-      posY = yaxis * SPRITESIZE;
-      if (scenemap.axis[xaxis] == null) {
-        scenemap.axis[xaxis] = [];
-      }
-      if (scenemap.axis[xaxis][yaxis] == null) {
-        scenemap.axis[xaxis][yaxis] = [];
-      }
-      return scenemap.axis[xaxis][yaxis].push(Crafty.e("2D, DOM, log").attr({
-        x: posX,
-        y: posY
-      }));
-    };
-    _fn = function(x) {
-      var y, _j, _ref1, _results;
-      _results = [];
-      for (y = _j = 0, _ref1 = scenemap.h - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; y = 0 <= _ref1 ? ++_j : --_j) {
-        _results.push(drawBackground(x, y));
-      }
-      return _results;
-    };
-    for (x = _i = 0, _ref = scenemap.w - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; x = 0 <= _ref ? ++_i : --_i) {
-      _fn(x);
-    }
-    (function() {
-      var logs, _results;
-      logs = LOGS + 1;
-      _results = [];
-      while ((logs -= 1)) {
-        _results.push(addLog());
-      }
-      return _results;
-    })();
-    return Crafty.e("2D, DOM, player, Human, LeftControls").attr({
-      x: 0,
-      y: 0,
-      z: 10
-    }).leftControls(1);
-  };
 
 }).call(this);
